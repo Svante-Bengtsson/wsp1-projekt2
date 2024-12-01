ccce81dd13b3bd5b3993703777aa0cda2fcb232d
Svante-Bengtsson <152278814+Svante-Bengtsson@users.noreply.github.com>
2024-12-01 21:43:34 +0100

    01/12/2024
    missade ladda upp har haft för mycket annat och varit sjuk väldigt mycket
    
    delvis crud färdighet, update och delete finns inte än men det går på 2 min, sen ska jag också lägga till användare och eventuellt även olika spel

--- app.rb
+++ app.rb
@@ -1,7 +1,34 @@
 class App < Sinatra::Base
 
-    get '/' do
-        erb(:"index")
+    def db
+        return @db if @db
+        @db = SQLite3::Database.new('db/stratroulette.sqlite')
+        @db.results_as_hash = true
+        return @db
     end
 
+    get '/' do
+        @strats = db.execute('SELECT * FROM strats') 
+        check = false
+        numb = rand(0...@strats.length)
+        @strat = @strats[numb]
+        erb(:"stratroulette/index")
+    end
+
+    get '/strats' do
+        @strats = db.execute('SELECT * FROM strats')
+        erb(:"stratroulette/strats")
+    end
+    get '/strats/:id' do | id |
+        @strats = db.execute('SELECT * FROM strats WHERE id = ?', id)
+        @strat = @strats[0]
+        p @strat
+        erb(:"stratroulette/show")
+    end
+
+        post '/ratings_update/:id' do | id |
+            p "Form submitted with ID: #{id}, Params: #{params.inspect}"
+            db.execute("UPDATE strats SET rating_tot = rating_tot + ?, rating_amount = rating_amount + 1 WHERE id = ?", [params['rating_tot'], id])
+            redirect("/")
+        end
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -3,9 +3,35 @@ require 'sqlite3'
 class Seeder
 
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS strats')
+    db.execute('DROP TABLE IF EXISTS users')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE strats (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                description TEXT NOT NULL,
+                rating_tot INTEGER,
+                rating_amount INTEGER DEFAULT 1)')
+  end 
+
+  def self.populate_tables
+    db.execute('INSERT INTO strats (name, description, rating_tot) VALUES ("gloob", "ma schloob", 0)')
+    db.execute('INSERT INTO strats (name, description, rating_tot) VALUES ("goop", "ma schoop", 5)')
+  end
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/stratroulette.sqlite')
+    @db.results_as_hash = true
+    @db
+  end
 end
 
 Seeder.seed!
